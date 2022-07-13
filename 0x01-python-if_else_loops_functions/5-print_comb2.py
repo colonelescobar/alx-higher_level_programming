572b18a90857cf97cc220d23c5de601899c4f5f9
colonelescobar <tiagat@yahoo.com>
2022-07-13 22:11:41 +0100

    5-print_cumbo

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-   if i < 99:
-       print("{},".format(i), end=" ")
+   if i < 99: 
+      print(f"{i:0>2d},", end=" ")
    else:
-    print("{}".format(i))
+    print(f"{i}")
