d5939b7113a9ef4ba11aaeddb472e6f9537ffb30
colonelescobar <tiagati@yahoo.com>
2022-10-04 00:18:27 +0100

    sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,2 +1,7 @@
 -- A script that creates a table 'second_table
-CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT)
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bobx", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "Georgex", 8);
+
