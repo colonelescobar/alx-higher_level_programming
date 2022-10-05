06fbf318e9e140668d75242bd2f7a80db28f18c4
colonelescobar <tiagati@yahoo.com>
2022-10-05 17:42:23 +0100

    SQL excercises

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- an sql script that lists all the records of the table second_table of the database.
-SELECT score, name FROM hbtn_0c_0.second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
