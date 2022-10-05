0c7f23afd760c0a3b8bee6bbcfebde3add61391b
colonelescobar <tiagati@yahoo.com>
2022-10-05 21:14:10 +0100

    sql queries

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- An sql script that displays the average temperature by city ordered by temperature.
-SELECT city, AVG(value) as avg_temp from hbtn_0c_0.temperatures GROUP BY city ORDER BY 2 DESC;
+SELECT city, AVG(value) AS avg_temp FROM hbtn_0c_0.temperatures GROUP BY city ORDER BY 2 DESC;
