e316eed0ff007fcd5732abab1842150acea2c4bc
colonelescobar <tiagati@yahoo.com>
2022-10-05 17:45:00 +0100

    sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- An SQL script that updates the score of Bob to 1- in the table 'second_table'.
-UPDATE hbtn_0c_0.second_table SET score = 10 WHERE name = 'Bob';
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
