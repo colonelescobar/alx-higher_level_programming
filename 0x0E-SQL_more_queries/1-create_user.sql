10aebdebb917a798ddd7c92b16f99627f438069a
colonelescobar <tiagati@yahoo.com>
2022-10-07 00:19:27 +0100

    privy

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- A script that creats a user user_0d_1 wiht all privilges.
+
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
