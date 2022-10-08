f1767db86fa8a8e4d7b85bb8fdca9aacae913749
colonelescobar <tiagati@yahoo.com>
2022-10-08 21:22:40 +0100

    right join

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,3 @@
+-- A script that shows all shows wnd their genres.
 SELECT tv_shows.title,genre_id FROM tv_show_genres RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id ORDER
 BY 1,2;      
