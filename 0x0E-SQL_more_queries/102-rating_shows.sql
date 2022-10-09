395b11c99105b99ddf0719daaad15326dac8d427
colonelescobar <tiagati@yahoo.com>
2022-10-09 16:15:03 +0100

    show _ratings

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,4 +1,4 @@
 -- This script lists all shows from the hbtn+-d+tvshows_rate by thir rating.
 
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) FROM tv_shows JOIN tv_show_ratings ON tv_show_ratings.show_id =
+SELECT tv_shows.title title, SUM(tv_show_ratings.rate) rating FROM tv_shows JOIN tv_show_ratings ON tv_show_ratings.show_id =
 tv_shows.id GROUP BY 1 ORDER BY 2 DESC;
