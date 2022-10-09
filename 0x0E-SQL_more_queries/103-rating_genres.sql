cd656d06fdd28533026186dff931a0da71bcb737
colonelescobar <tiagati@yahoo.com>
2022-10-09 17:03:52 +0100

    4 join SQL queries

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,3 @@
+-- A script that lists all genres in the database hbtn_0d_tvshows_rate
+
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) FROM tv_shows JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id JOIN tv_show_genres ON tv_show_genres.show_id = tv_show_ratings.show_id JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY 1 ORDER BY 2 DESC;
