eced584c932b548e2867be87809530560e53a5ef
colonelescobar <tiagati@yahoo.com>
2022-10-09 15:33:26 +0100

    101-not_a_comedy

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,4 +1,3 @@
 -- A sccript that lists all shows without the genre "Comedy" in the databse hbtn_0d_tvshows.
 
-SELECT tv_shows.title FROM tv_shows WHERE tv_shows.title NOT IN (SELECT tv_shows.title FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id JOIN tv_genres ON t
-	v_show_genres.genre_id = tv_genres.id  WHERE tv_genres.name = 'Comedy') ORDER BY 1 ASC;
+SELECT tv_shows.title FROM tv_shows WHERE tv_shows.title NOT IN (SELECT tv_shows.title FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id  WHERE tv_genres.name = 'Comedy') ORDER BY 1 ASC;
