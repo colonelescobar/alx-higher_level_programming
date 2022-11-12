e90d5156f0548c9f1935d5b1bfeedf621c90a71f
colonelescobar <tiagati@yahoo.com>
2022-11-12 19:23:24 +0100

    added 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,7 @@
-#!/usr/bin/node 
+#!/usr/bin/node
 
-console.log('My number:' + )
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
