f1ad5f492f2e7054e2edfa342a794f4967dee090
colonelescobar <tiagati@yahoo.com>
2022-11-12 19:45:44 +0100

    6-multi_languages_loop.js added

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
+const stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
+for (let counter = 0; counter < stringArray.length; counter++) {
+  console.log(stringArray[counter]);
+}
