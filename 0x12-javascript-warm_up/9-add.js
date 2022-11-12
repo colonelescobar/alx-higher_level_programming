3dd772aa64b5e18acd5d01a06855bc5bc160f5d6
colonelescobar <tiagati@yahoo.com>
2022-11-12 20:46:15 +0100

    9-add.js added

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const value = process.argv;
+function add (a, b) {
+  const value = process.argv;
+  console.log(parseInt(value[2]) + parseInt(value[3]));
+}
+
+add(value[2], value[3]);
