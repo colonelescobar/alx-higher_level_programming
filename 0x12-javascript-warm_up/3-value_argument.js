4d5a51e1a56032c19c0bb356aa36047cfc554300
colonelescobar <tiagati@yahoo.com>
2022-11-12 11:22:17 +0100

    added 3-value_arguements

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv[2] !== undefined ) {
-	  console.log(process.argv[2]);
+if (process.argv[2] !== undefined) {
+  console.log(process.argv[2]);
 } else {
-	  console.log('No argument');
-} 
+  console.log('No argument');
+}
