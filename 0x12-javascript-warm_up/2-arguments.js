47cfd7539bace59019c1a197c3016937d8db49be
colonelescobar <tiagati@yahoo.com>
2022-11-12 11:03:04 +0100

    added 2-arguements file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv.length < 3) {
+if (process.argv.length <= 2) {
   console.log('No argument');
-} else if (process.argv.length > 2) {
-  console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
 } else {
-  console.log('Error');
+  console.log('Arguement found');
 }
