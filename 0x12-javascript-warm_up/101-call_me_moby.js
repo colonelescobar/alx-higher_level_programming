c556c0624679db1a07cbc795bbdfe09d26ae4635
colonelescobar <tiagati@yahoo.com>
2022-11-20 13:05:07 +0100

    101-call_me_moby.js added

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+exports.callMeMoby = function (x, theFunction) { 
+  for (let a= 0; a < x; a++){
+    theFunction();
+  }
+};
---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+/*
+ * YOUR CODE HERE
+ :wq
+
+	*/
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
